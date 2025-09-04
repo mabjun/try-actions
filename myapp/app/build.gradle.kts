e6a070c6b3a0cfcd517ad4cefc9e10eb058d36f4
Jun Mabuchi <mab@mabware.jp>
2025-09-04 18:21:03 +0900

    4つのflavorを別署名でビルドできるところまで。

--- myapp/app/build.gradle.kts
+++ myapp/app/build.gradle.kts
@@ -5,18 +5,60 @@ plugins {
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("production") {
+            val productionStoreFile: String by project
+            val productionStorePassword: String by project
+            val productionKeyAlias: String by project
+            val productionKeyPassword: String by project
+            storeFile = file(productionStoreFile)
+            storePassword = productionStorePassword
+            keyAlias = productionKeyAlias
+            keyPassword = productionKeyPassword
+        }
+        create("staging") {
+            val stagingStoreFile: String by project
+            val stagingStorePassword: String by project
+            val stagingKeyAlias: String by project
+            val stagingKeyPassword: String by project
+            storeFile = file(stagingStoreFile)
+            storePassword = stagingStorePassword
+            keyAlias = stagingKeyAlias
+            keyPassword = stagingKeyPassword
+        }
+        create("sandbox") {
+            val sandboxStoreFile: String by project
+            val sandboxStorePassword: String by project
+            val sandboxKeyAlias: String by project
+            val sandboxKeyPassword: String by project
+            storeFile = file(sandboxStoreFile)
+            storePassword = sandboxStorePassword
+            keyAlias = sandboxKeyAlias
+            keyPassword = sandboxKeyPassword
+        }
+        create("develop") {
+            val developStoreFile: String by project
+            val developStorePassword: String by project
+            val developKeyAlias: String by project
+            val developKeyPassword: String by project
+            storeFile = file(developStoreFile)
+            storePassword = developStorePassword
+            keyAlias = developKeyAlias
+            keyPassword = developKeyPassword
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -35,6 +77,32 @@ android {
     }
     buildFeatures {
         viewBinding = true
+    }
+
+    flavorDimensions += listOf("default")
+    productFlavors {
+        create("production") {
+            dimension = "default"
+            signingConfig = signingConfigs.getByName("production")
+        }
+        create("staging") {
+            dimension = "default"
+            applicationIdSuffix = "staging"
+            versionNameSuffix = "staging"
+            signingConfig = signingConfigs.getByName("staging")
+        }
+        create("sandbox") {
+            dimension = "default"
+            applicationIdSuffix = "sandbox"
+            versionNameSuffix = "sandbox"
+            signingConfig = signingConfigs.getByName("sandbox")
+        }
+        create("develop") {
+            dimension = "default"
+            applicationIdSuffix = "develop"
+            versionNameSuffix = "develop"
+            signingConfig = signingConfigs.getByName("develop")
+        }
     }
 }
 
