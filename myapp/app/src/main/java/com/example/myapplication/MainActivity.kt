1367dcfc3639607d7a8cfc3a61a521dd4706dbe6
Jun Mabuchi <mab@mabware.jp>
2025-10-01 14:55:24 +0900

    abc

--- myapp/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ myapp/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    var abc: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
