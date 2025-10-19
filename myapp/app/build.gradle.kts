bd3bdc793baeb3229f755ba4869553b5220da25d
Jun Mabuchi <mab@mabware.jp>
2025-10-19 17:10:36 +0900

    200

--- myapp/app/build.gradle.kts
+++ myapp/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
